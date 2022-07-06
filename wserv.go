b79aef909bcad5fbc608eb0f5a992baf2cc4a38b
Mauro Pisu <pisu.maru@gmail.com>
2022-07-06 18:20:18 +0200

    Read

--- wserv.go
+++ wserv.go
@@ -1,77 +1,96 @@
 package main
 
 import (
- 	"os"
+	"database/sql"
+	"encoding/json"
 	"fmt"
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/gorilla/mux"
+	"github.com/tkanos/gonfig"
 	"log"
 	"net/http"
-	"encoding/json"
-	"github.com/gorilla/mux"
-	"database/sql"
-	_ "github.com/go-sql-driver/mysql"
 )
 
-type Person struct{
-	FirstName string `json:"firstName"`
+type Person struct {
+	FirstName  string `json:"firstName"`
 	SecondName string `json:"secondName"`
 }
 
 var db *sql.DB
 var err error
-var username string
-var password string
+var configuration Config
 
-func homePage(w http.ResponseWriter, r *http.Request){
+type Config struct {
+	DB_USERNAME string
+	DB_PASSWORD string
+	DB_PORT     string
+	DB_HOST     string
+	DB_NAME     string
+}
+
+func GetConfiguration() Config {
+	config := Config{}
+	fileName := "./conf.json"
+	gonfig.GetConf(fileName, &config)
+	return config
+}
+
+func homePage(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Welcome to the HomePage!")
 	fmt.Println("Endpoint Hit: homePage")
 }
 
-func returnAllArticles(w http.ResponseWriter, r *http.Request){
-  	params := mux.Vars(r)
-    	table := params["table"]
-	fmt.Println("table: %s",table)
-	        db, err := sql.Open("mysql", username + ":" + password + "@tcp(127.0.0.1:3306)/test")
-        defer db.Close()
-        if err != nil {
-                panic(err.Error()) // Just for example purpose. You should use proper error handling instead of panic
-        }
+func read(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
+	table := params["table"]
+	fmt.Println("table: %s", table)
+	db, err := sql.Open("mysql", configuration.DB_USERNAME+":"+configuration.DB_PASSWORD+"@tcp(127.0.0.1:3306)/test")
+	defer db.Close()
+	if err != nil {
+		panic(err.Error()) // Just for example purpose. You should use proper error handling instead of panic
+	}
 
-        err = db.Ping()
-        if err != nil {
-                panic(err.Error()) // Just for example purpose. You should use proper error handling instead of panic
-        }
+	err = db.Ping()
+	if err != nil {
+		panic(err.Error()) // Just for example purpose. You should use proper error handling instead of panic
+	}
 
-        fmt.Println("succesfully connected to mysql", err)
-
+	fmt.Println("succesfully connected to mysql", err)
 
 	rows, err := db.Query(`SELECT * FROM ` + table)
 
-    defer rows.Close()
-    var rowBuf, _ = rows.Columns()
-    var cols = make([]string, len(rowBuf))
-    copy(cols, rowBuf)
-    fmt.Println(rowBuf)
-    var vals = make([]interface{}, len(rowBuf))
-    for i, _ := range rowBuf {
-        vals[i] = &rowBuf[i]
-    }
-    for rows.Next() {
-        err := rows.Scan(vals...)
-        if err != nil {
-            log.Fatal(err)
-        }
-        var m = map[string]interface{}{}
-        for i, col := range cols {
-            m[col] = vals[i]
-        }
-        obj, _ := json.Marshal(m)
-        //
-        fmt.Fprintf(w, string(obj))
-    }
-    err = rows.Err()
-    if err != nil {
-        log.Fatal(err)
-    }
+	defer rows.Close()
+	var rowBuf, _ = rows.Columns()
+	var cols = make([]string, len(rowBuf))
+	copy(cols, rowBuf)
+	fmt.Println(rowBuf)
+	var vals = make([]interface{}, len(rowBuf))
+	for i, _ := range rowBuf {
+		vals[i] = &rowBuf[i]
+	}
+	fmt.Fprintf(w, "{\"data\":[")
+	var index = 0
+	for rows.Next() {
+		err := rows.Scan(vals...)
+		if err != nil {
+			log.Fatal(err)
+		}
+		var m = map[string]interface{}{}
+		for i, col := range cols {
+			m[col] = vals[i]
+		}
+		obj, _ := json.Marshal(m)
+		if index != 0 {
+			fmt.Fprintf(w, ",")
+		}
+		index = index + 1
+		fmt.Fprintf(w, string(obj))
+	}
+	fmt.Fprintf(w, "]}")
+	err = rows.Err()
+	if err != nil {
+		log.Fatal(err)
+	}
 }
 
 func Middleware(next http.Handler) http.Handler {
@@ -91,17 +110,14 @@ func Middleware(next http.Handler) http.Handler {
 
 func handleRequests() {
 	router := mux.NewRouter().StrictSlash(true)
-	router.Use(Middleware);
+	router.Use(Middleware)
 	router.HandleFunc("/", homePage)
-	router.HandleFunc("/table/{table:[a-z]+}", returnAllArticles)
+	router.HandleFunc("/table/{table:[a-z]+}", read)
 	log.Fatal(http.ListenAndServe(":10000", router))
 
 }
 
 func main() {
-
-	username = os.Args[1]
-	password = os.Args[1]
-
+	configuration = GetConfiguration()
 	handleRequests()
 }
